0075fcad6f4c9f4608f1f4c7dcfe74feac0db8b6
Alex Marion <ajm482@drexel.edu>
2017-02-24 22:09:08 -0500

    Created collaboration filtering functions

--- cf_test/collaborative_filtering.py
+++ cf_test/collaborative_filtering.py
@@ -3,75 +3,88 @@ from scipy.spatial.distance import cosine
 from data_converter import get_data
 
 '''~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ DATA LOADING ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~'''
-[user_ids, product_names, data_raw] = get_data()
-data = pd.DataFrame(data_raw)
-data.columns = product_names
-data.insert(0,'user',user_ids)
+def collab_filter_load_data():
+	[user_ids, product_names, data_raw] = get_data()
+	data = pd.DataFrame(data_raw)
+	data.columns = product_names
+	data.insert(0,'user',user_ids)
 
-#data = pd.read_csv('test_data.csv')
-#print(data.head(6).ix[:,2:8])
+	#data = pd.read_csv('test_data.csv')
+	#print(data.head(6).ix[:,2:8])
 
-# Drop user column
-data_raw = data.drop('user',1)
-print('Raw Data')
-print(data)
-print()
+	# Drop user column
+	data_raw = data.drop('user',1)
+	print('Raw Data')
+	print(data)
+	print()
+	return data
 
 
 
 '''~~~~~~~~~~~~~~~~~~~~~ ITEM BASED COLLABORATIVE FILTERING ~~~~~~~~~~~~~~~~~~~~'''
-# Create placeholder dataframe listing item vs. item
-data_ibs = pd.DataFrame(index=data_raw.columns,columns=data_raw.columns)
+def item_collab_filter():
+	data = collab_filter_load_data()
+	data_raw = data.drop('user',1)
+	# Create placeholder dataframe listing item vs. item
+	data_ibs = pd.DataFrame(index=data_raw.columns,columns=data_raw.columns)
 
-# Fill placeholder with cosine similarities
-for i in range(0,len(data_ibs.columns)):
-	for j in range(0,len(data_ibs.columns)):
-		data_ibs.ix[i,j] = 1-cosine(data_raw.ix[:,i],data_raw.ix[:,j])
+	# Fill placeholder with cosine similarities
+	for i in range(0,len(data_ibs.columns)):
+		for j in range(0,len(data_ibs.columns)):
+			data_ibs.ix[i,j] = 1-cosine(data_raw.ix[:,i],data_raw.ix[:,j])
 
-# Create a placeholder dataframe for closest neighbours to an item
-num_cols = len(data_ibs.columns)
-data_neighbours = pd.DataFrame(index=data_ibs.columns,columns=range(1,num_cols + 1))
+	# Create a placeholder dataframe for closest neighbours to an item
+	num_cols = len(data_ibs.columns)
+	data_neighbours = pd.DataFrame(index=data_ibs.columns,columns=range(1,num_cols + 1))
 
-# Loop through similarity dataframe and fill in neighbouring item names
-for i in range(0,len(data_ibs.columns)):
-	data_neighbours.ix[i,:num_cols] = data_ibs.ix[0:,i].sort_values(ascending=False)[:num_cols].index
+	# Loop through similarity dataframe and fill in neighbouring item names
+	for i in range(0,len(data_ibs.columns)):
+		data_neighbours.ix[i,:num_cols] = data_ibs.ix[0:,i].sort_values(ascending=False)[:num_cols].index
 
-print('Item Based Collaborative Filtering')
-print(data_neighbours);
-print()
+	print('Item Based Collaborative Filtering')
+	print(data_neighbours);
+	print()
+	return [data_neighbours, data_ibs]
 
 
 '''~~~~~~~~~~~~~~~~~~~~~ USER BASED COLLABORATIVE FILTERING ~~~~~~~~~~~~~~~~~~~~'''
 # Helper function to get similarities
-def getScore(history, similarities):
+def get_score(history, similarities):
 	return sum(history*similarities)/sum(similarities)
 
-# Create a placeholder for similarities and fill in user names
-data_sims = pd.DataFrame(index=data.index,columns=data.columns)
-data_sims.ix[:,:1] = data.ix[:,:1]
+def user_collab_filter(): 
+	data = collab_filter_load_data()
+	data_raw = data.drop('user',1)
+	[data_neighbours, data_ibs] = item_collab_filter()
 
-# Fill similarity scores (skipping user column)
-for i in range(0,len(data_sims.index)):
-	for j in range(1,len(data_sims.columns)):
-		user = data_sims.index[i]
-		product = data_sims.columns[j]
+	# Create a placeholder for similarities and fill in user names
+	data_sims = pd.DataFrame(index=data.index,columns=data.columns)
+	data_sims.ix[:,:1] = data.ix[:,:1]
 
-		if data.ix[i][j] == 1:
-			data_sims.ix[i][j] = 0
-		else:
-			product_top_names = data_neighbours.ix[product][1:10]		
-			product_top_sims = data_ibs.ix[product].sort_values(ascending=False)
-			user_purchases = data_raw.ix[user,product_top_names]
+	# Fill similarity scores (skipping user column)
+	for i in range(0,len(data_sims.index)):
+		for j in range(1,len(data_sims.columns)):
+			user = data_sims.index[i]
+			product = data_sims.columns[j]
 
-			data_sims.ix[i][j] = getScore(user_purchases,product_top_sims)
+			if data.ix[i][j] == 1:
+				data_sims.ix[i][j] = 0
+			else:
+				product_top_names = data_neighbours.ix[product][1:10]		
+				product_top_sims = data_ibs.ix[product].sort_values(ascending=False)
+				user_purchases = data_raw.ix[user,product_top_names]
 
-# Get top products
-data_recommend = pd.DataFrame(index=data_sims.index,columns=['user','1','2','3','4','5','6'])
-data_recommend.ix[0:,0] = data_sims.ix[:,0]
+				data_sims.ix[i][j] = get_score(user_purchases,product_top_sims)
 
-# Assign product names to top scores
-for i in range(0,len(data_sims.index)):
-	data_recommend.ix[i,1:] = data_sims.ix[i,:].sort_values(ascending=False).ix[1:7,].index.transpose()
+	# Get top products
+	data_recommend = pd.DataFrame(index=data_sims.index,columns=['user','1','2','3','4','5','6'])
+	data_recommend.ix[0:,0] = data_sims.ix[:,0]
 
-print('User Based Collaborative Filtering')
-print(data_recommend.ix[:10,:7])
+	# Assign product names to top scores
+	for i in range(0,len(data_sims.index)):
+		data_recommend.ix[i,1:] = data_sims.ix[i,:].sort_values(ascending=False).ix[1:7,].index.transpose()
+
+	print('User Based Collaborative Filtering')
+	print(data_recommend.ix[:10,:7])
+
+	return data_recommend
---
+++ cf_test/test.py
@@ -0,0 +1,3 @@
+from collaborative_filtering import item_collab_filter, user_collab_filter
+item_collab_filter()
+user_collab_filter()
