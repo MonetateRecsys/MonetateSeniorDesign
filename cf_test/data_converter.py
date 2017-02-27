2ca94ce5aeedad19cee494c2714a7c5feeb95f45
Tomer Shemesh <tomerjshemesh@gmail.com>
2017-02-27 13:59:39 -0500

    map

--- cf_test/data_converter.py
+++ cf_test/data_converter.py
@@ -2,41 +2,49 @@ import pandas as pd
 import data_loader
 from scipy.spatial.distance import cosine
 
+
 def get_data():
-	conn = data_loader.get_connection()
-	data_loader.init_database(conn)
-	data_loader.load_fake_data(conn)
+    conn = data_loader.get_connection()
+    data_loader.init_database(conn)
+    data_loader.load_fake_data(conn)
 
-	#data_loader.get_users(conn)
-	#print(data_loader.get_all_data(conn))
+    catalogs = data_loader.get_catalogs(conn)
+    print(catalogs)
+    catalog_id = catalogs[0][0]
+    users_with_products = data_loader.get_products_bought(conn, catalog_id)
 
-	#print(pd.DataFrame(data_loader.get_all_data(conn)))
-	catalogs = data_loader.get_catalogs(conn)
-	print(catalogs)
-	catalog_id = catalogs[0][0]
-	users = data_loader.get_users(conn)
-	products = data_loader.get_products(conn, catalog_id)
-	user_names = [user[0] for user in users]
-	product_names = [product[3] for product in products]
+    user_ids = set([trans[1] for trans in users_with_products])
+    product_ids = set([trans[2] for trans in users_with_products])
 
-	users_with_products = data_loader.get_products_bought(conn, catalog_id)
+    userlookup = {}
+    productlookup = {}
+    productIdlookup = {}
+    userIdlookup = {}
 
-	user_ids = [trans[1] for trans in users_with_products]
-	product_ids = [trans[2] for trans in users_with_products]
+    maxUser = 0
+    maxProduct = 0
 
-	product_offset =max(product_ids) - min(product_ids)+1
-	user_offset = max(user_ids) - min(user_ids)+1
+    matrix = [[0 for x in range(len(product_ids))] for y in range(len(user_ids))]
+    for trans in users_with_products:
+        if(trans[1] in userlookup):
+            user_index = userlookup[trans[1]]
+        else:
+            userlookup[trans[1]] = maxUser
+            userIdlookup[maxUser] = trans[1]
+            user_index = maxUser
+            maxUser += 1
 
-	#print(product_offset)
-	#print(user_offset)
-	#print(data_loader.get_products(conn))
-	#print(product_ids)
+        if(trans[2] in productlookup):
+            prod_index = productlookup[trans[2]]
+        else:
+            productlookup[trans[2]] = maxProduct
+            productIdlookup[maxProduct] = trans[2]
+            prod_index = maxProduct
+            maxProduct += 1
 
-	matrix = [[0 for x in range(product_offset)] for y in range(user_offset)]
-	for trans in users_with_products:
-		matrix[trans[1]-min(user_ids)][trans[2]-min(product_ids)] = 1
+        matrix[user_index][prod_index] = 1
 
-	conn.commit()
-	conn.close()
+    conn.commit()
+    conn.close()
 
-	return [user_names, product_names, matrix]
+    return [userIdlookup, productIdlookup, matrix]
